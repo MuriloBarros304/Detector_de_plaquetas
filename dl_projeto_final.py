75ba3b859b1578946f990af0763097a414943af3
Murilo Barros <murilobarros304@gmail.com>
2024-05-23 16:39:48 -0300

    update

--- dl_projeto_final.py
+++ dl_projeto_final.py
@@ -2,21 +2,21 @@ import cv2
 from ultralytics import YOLO
 
 # Load the pre-trained YOLOv5 model
-model = YOLO('yolo8n.pt')
+model = YOLO('best.pt')
 
 # Function to perform object detection on a single frame
 def detect_objects(frame):
     # Perform object detection on the frame
-    results = model.predict(frame)
+    #frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    results = model.track(source=frame, conf=0.5, max_det=1, show_labels=False)
 
     # Draw bounding boxes and labels on the frame
     for box in results[0].boxes:
         x1, y1, x2, y2 = box.xyxy[0]
-        label = f'{model.names[box.cls][0]} {box.conf:.2f}'
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
-        cv2.putText(frame, label, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-    return frame
+    #frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+    #print(results)
+    return frame 
 
 # Open the video file
 video_path = 'video_teste2.mp4'
@@ -41,10 +41,10 @@ while cap.isOpened():
     out.write(frame_with_detections)
 
     # Display the frame with detections (optional)
-    cv2.imshow('frame', frame_with_detections)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+    #cv2.imshow('frame', frame_with_detections)
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #break
+    
 # Release the resources
 cap.release()
 out.release()
